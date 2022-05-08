b1de5aab352b9718073798c9360b8533e9307a9e
kanedu828 <kanedu2000@gmail.com>
2022-05-08 17:53:32 -0400

    add timeline button to home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,6 +2,7 @@ import '../styles/home.css'
 import '../styles/shared.css';
 
 import { useMediaQuery } from 'react-responsive'
+import  { NavLink } from "react-router-dom";
 
 
 function Home() {
@@ -13,11 +14,12 @@ function Home() {
       <div className='container-col fade-in'>
         <h1 className='large-header'>About Me</h1>
         <p className='about-container content'>
-          Hello! I am a 4th year Computer Science major at
+          Hello! My name is Kane and I am a 4th year Computer Science major at
           Georgia Tech. My concetrations are in intelligence and media.
           I will be graduating in December 2022.
-          You can take a look at my coursework and internships in the timeline section.
+          You can take a look at my coursework and internships in my timeline.
         </p>
+        <NavLink className='main-button' to='/timeline'> See My Timeline!  </NavLink>
       </div>
       <img className='about-image' src={require('../assets/images/about_drawing.png')}/>
     </div>
